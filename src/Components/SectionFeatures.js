6ab618b12e5997b1d8a4bef7b27e0ccf60b09dea
Greyland-TM <greyland.official@gmail.com>
2021-01-10 11:30:10 -0800

    new quearies

--- src/Components/SectionFeatures.js
+++ src/Components/SectionFeatures.js
@@ -3,47 +3,55 @@ import '../css/icon-font.css';
 
 const Features = () => (
     <section className="section-features">
+
         <div className="row">
             <div className="col-1-of-4">
+                <div className='feature-box-container'>
                 <div className="feature-box">
                     
-                <i className='feature-box__icon icon-basic-world'></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
-                <p className="feature-box__text">Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Porro sed saepe sint repellat 
-                autem qui adipisci rem praesentium.
-                </p>
+                    <i className='feature-box__icon icon-basic-world'></i>
+                    <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
+                    <p className="feature-box__text">Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Porro sed saepe sint repellat 
+                        autem qui adipisci rem praesentium.
+                    </p>
 
+                </div>
                 </div>
             </div>
 
             <div className="col-1-of-4">
+            <div className='feature-box-container'>
                 <div className="feature-box">
                     
                 <i className='feature-box__icon icon-basic-compass'></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Meet nature</h3>
+                <h3 className="feature-box__head heading-tertiary u-margin-bottom-small">Meet nature</h3>
                 <p className="feature-box__text">Lorem ipsum dolor sit amet 
                 consectetur adipisicing elit. Porro sed saepe sint repellat 
                 autem qui adipisci rem praesentium.
                 </p>
 
                 </div>
-            </div>
-
-            <div className="col-1-of-4">
-                <div className="feature-box">
-                    
-                <i className='feature-box__icon icon-basic-map'></i>
-                <h3 className="heading-tertiary u-margin-bottom-small">Find your way</h3>
-                <p className="feature-box__text">Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Porro sed saepe sint repellat 
-                autem qui adipisci rem praesentium.
-                </p>
-
                 </div>
             </div>
 
             <div className="col-1-of-4">
+                <div className='feature-box-container'>
+                    <div className="feature-box">
+                        
+                    <i className='feature-box__icon icon-basic-map'></i>
+                    <h3 className="heading-tertiary u-margin-bottom-small">Find your way</h3>
+                    <p className="feature-box__text">Lorem ipsum dolor sit amet 
+                    consectetur adipisicing elit. Porro sed saepe sint repellat 
+                    autem qui adipisci rem praesentium.
+                    </p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-1-of-4">
+            <div className='feature-box-container'>
                 <div className="feature-box">
                     
                 <i className='feature-box__icon icon-basic-heart'></i>
@@ -54,6 +62,7 @@ const Features = () => (
                 </p>
 
                 </div>
+            </div>
             </div>
         </div>
     </section>
